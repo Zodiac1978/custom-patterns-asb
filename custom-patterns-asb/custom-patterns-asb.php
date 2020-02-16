26ebde74618f0e68813de7fa943e4f45751b13e3
Torsten Landsiedel <torsten.landsiedel@gmx.de>
2020-02-16 14:27:47 +0100

    Added some patterns and coding standards

--- custom-patterns-asb/custom-patterns-asb.php
+++ custom-patterns-asb/custom-patterns-asb.php
@@ -2,65 +2,92 @@
 /**
  * Plugin Name: Custom Patterns for Antispam Bee
  * Description: Add custom patterns for Antispam Bee.
- * Plugin URI:  http://torstenlandsiedel.de
- * Version:     1.2
+ * Plugin URI:  https://torstenlandsiedel.de
+ * Version:     1.3
  * Author:      Torsten Landsiedel
- * Author URI:  http://torstenlandsiedel.de
+ * Author URI:  https://torstenlandsiedel.de
  * Licence:     GPL 2
  * License URI: http://opensource.org/licenses/GPL-2.0
  * GitHub Plugin URI: https://github.com/Zodiac1978/custom-patterns-asb
  */
- 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 
 /**
  * Antispam Bee filter for custom RegExp patterns
  */
-add_filter( 'antispam_bee_patterns', 'antispam_bee_add_custom_patterns'	);
+add_filter( 'antispam_bee_patterns', 'antispam_bee_add_custom_patterns' );
 
-
-function antispam_bee_add_custom_patterns($patterns) {
-	// body text with two or less characters
+/**
+ * Add more RegExp patterns
+ *
+ * @param   Array $patterns All RegExp patterns from ASB.
+ */
+function antispam_bee_add_custom_patterns( $patterns ) {
+	// Body text with two or less characters.
 	$patterns[] = array(
 		'body' => '^(?=.{0,2}$).*',
 	);
 
-	// spammy email addresses (gmail would be here too, but this is not useful for an european blog)
+	// Spammy email addresses (gmail would be here too, but this is not useful for an european blog).
 	$patterns[] = array(
-		'email'	=> '@mail\.ru|@yandex\.$',
+		'email' => '@mail\.ru|@yandex\.$',
 	);
 
-	// every comment with .ru/.bid top level domain
-	// See: http://www.online-erfolgreich.net/webseite-und-technik/spam-bekaempfen-mit-antispam-bee-und-regulaere-ausdruecke-via-plugin-hook/
+	// every comment with .ru/.bid top level doman
+	// @link: http://www.online-erfolgreich.net/webseite-und-technik/spam-bekaempfen-mit-antispam-bee-und-regulaere-ausdruecke-via-plugin-hook/
 	$patterns[] = array(
-		'email' => '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.(ru|bid)+$)'
+		'email' => '(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.(ru|bid)+$)',
 	);
 
-	// spam text in email
+	// Spam text in author name.
+	$patterns[] = array(
+		'author' => '(xxx|sex)(\d\d\d).(top|xyz)',
+	);
+	// Spam text in email.
+	$patterns[] = array(
+		'email' => '(xxx|sex)(\d\d\d).(top|xyz)',
+	);
+	// Spam text in hostname.
+	$patterns[] = array(
+		'host' => '(xxx|sex)(\d\d\d).(top|xyz)',
+	);
+	// Spam text in body.
+	$patterns[] = array(
+		'body' => '(xxx|sex)(\d\d\d).(top|xyz)',
+	);
+
+	// Crazy spam with exactly 10 chars in name and 30 chars in body.
+	$patterns[] = array(
+		'body' => '\b[a-z]{30}\b',
+		'name' => '\b[a-z]{10}\b',
+	);
+
+	// Spam text in email.
 	$patterns[] = array(
 		'email' => 'viagra|cialis|casino',
 	);
 
-	// spam text in host/url
+	// Spam text in host/url.
 	$patterns[] = array(
 		'host' => 'viagra|cialis|casino',
 	);
 
-	// spam text in body
+	// Spam text in body.
 	$patterns[] = array(
 		'body' => 'target[t]?ed (visitors|traffic)|viagra|cialis',
 	);
 
 	// 3 or more links in body
-	// See: http://www.online-erfolgreich.net/webseite-und-technik/spam-bekaempfen-mit-antispam-bee-und-regulaere-ausdruecke-via-plugin-hook/
+	// http://www.online-erfolgreich.net/webseite-und-technik/spam-bekaempfen-mit-antispam-bee-und-regulaere-ausdruecke-via-plugin-hook/
 	$patterns[] = array(
 		'body' => '(.*(http|https|ftp|ftps)\:\/\/){3,}',
 	);
 
 	// non latin characters (like Cyricllic, Japanese, etc.) in body
-	// http://www.regular-expressions.info/unicode.html
-	// If you have visitors in those languages just delete these from the pattern
+	// @link: http://www.regular-expressions.info/unicode.html
 	$patterns[] = array(
 		'body' => '\p{Arabic}|\p{Armenian}|\p{Bengali}|\p{Bopomofo}|\p{Braille}|\p{Buhid}|\p{Canadian_Aboriginal}|\p{Cherokee}|\p{Cyrillic}|\p{Devanagari}|\p{Ethiopic}|\p{Georgian}|\p{Greek}|\p{Gujarati}|\p{Gurmukhi}|\p{Han}|\p{Hangul}|\p{Hanunoo}|\p{Hebrew}|\p{Hiragana}|\p{Inherited}|\p{Kannada}|\p{Katakana}|\p{Khmer}|\p{Lao}|\p{Limbu}|\p{Malayalam}|\p{Mongolian}|\p{Myanmar}|\p{Ogham}|\p{Oriya}|\p{Runic}|\p{Sinhala}|\p{Syriac}|\p{Tagalog}|\p{Tagbanwa}|\p{Tamil}|\p{Telugu}|\p{Thaana}|\p{Thai}|\p{Tibetan}|\p{Yi}',
 	);
