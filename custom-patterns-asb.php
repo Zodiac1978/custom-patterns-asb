3fe06ababb55c469ae1328cd4fc0d3134524dc7b
Torsten Landsiedel <torsten.landsiedel@gmx.de>
2021-02-21 21:14:59 +0100

    Change folder structure

--- custom-patterns-asb.php
+++ custom-patterns-asb.php
@@ -86,7 +86,7 @@ function antispam_bee_add_custom_patterns( $patterns ) {
 		'body' => '(.*(http|https|ftp|ftps)\:\/\/){3,}',
 	);
 
-	// non latin characters (like Cyricllic, Japanese, etc.) in body
+	// non latin characters (like Cyrillic, Japanese, etc.) in body
 	// @link: http://www.regular-expressions.info/unicode.html
 	$patterns[] = array(
 		'body' => '\p{Arabic}|\p{Armenian}|\p{Bengali}|\p{Bopomofo}|\p{Braille}|\p{Buhid}|\p{Canadian_Aboriginal}|\p{Cherokee}|\p{Cyrillic}|\p{Devanagari}|\p{Ethiopic}|\p{Georgian}|\p{Greek}|\p{Gujarati}|\p{Gurmukhi}|\p{Han}|\p{Hangul}|\p{Hanunoo}|\p{Hebrew}|\p{Hiragana}|\p{Inherited}|\p{Kannada}|\p{Katakana}|\p{Khmer}|\p{Lao}|\p{Limbu}|\p{Malayalam}|\p{Mongolian}|\p{Myanmar}|\p{Ogham}|\p{Oriya}|\p{Runic}|\p{Sinhala}|\p{Syriac}|\p{Tagalog}|\p{Tagbanwa}|\p{Tamil}|\p{Telugu}|\p{Thaana}|\p{Thai}|\p{Tibetan}|\p{Yi}',
